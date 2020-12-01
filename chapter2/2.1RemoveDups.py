3319393faaae3c5129d388ab66ded98d0cd89f1a
samkarki <samkarkee@gmail.com>
2020-12-01 17:35:47 -0600

    Removing duplicate

---
+++ chapter2/2.1RemoveDups.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("")
+from LinkedList import ll
+
+# time complexity o(n), space complexity 0(n)
+def removeDups(alist):
+    elems = []
+
+    previous = None
+    current = alist.head
+    while current != None:
+        if current.data not in elems:
+            elems.append(current.data)
+            previous = current
+            current = current.next
+
+        else:
+            previous.next = current.next
+            current = current.next
+    return elems
+
+print(removeDups(ll))
+
+
+
