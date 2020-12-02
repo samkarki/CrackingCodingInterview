08c6b819a5d5c86851e59fe3e7b239c265d5c392
samkarki <samkarkee@gmail.com>
2020-12-01 23:24:14 -0600

    i guess i need little help here

---
+++ chapter2/2.3Delete_middleNode.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append(".")
+from LinkedList import *
+
+#time complexity O(n)
+def deleteMidnode(alist, node):
+    current = alist.head
+    previous = None
+
+
+    while current != None:
+        if current.data == node:
+            previous.next = current.next
+            current = current.next
+        previous= current
+        current = current.next
+
+print(ll.print_Linkedlist())
+deleteMidnode(ll, 3)
+print(ll.print_Linkedlist())
+
+#the code gives error when the node is first and last
+#not sure how to raise an error when the node is the first and last element
